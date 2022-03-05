229d8e9e56e665fbe0b13aedda6e3b38ea7d0c00
adc2108 <adc2108@rit.edu>
2022-03-05 15:39:21 -0500

    Initial Commit

---
+++ Shared/BestBudsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestBudsApp.swift
+//  Shared
+//
+//  Created by Alexandra Ciesielka on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BestBudsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
